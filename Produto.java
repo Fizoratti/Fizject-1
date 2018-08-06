e7b123d92000a0d7cecb2f3ac99643494254a6f9
fizoratti <robson.felipe@acad.pucrs.br>
2018-08-06 18:20:00 -0300

    Add Produto constructor

--- Produto.java
+++ Produto.java
@@ -2,4 +2,10 @@ public class Produto {
     private String codigo;
     private String nome;
     private float preco;
+
+    public Produto(String codigo, String nome, float preco){
+        this.codigo = codigo;
+        this.nome = nome;
+        this.preco = preco;
+    }
 }
